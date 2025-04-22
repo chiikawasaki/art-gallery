79235683025db7c6b8b8279b77180050561ee542
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-04-22 16:25:03 +0900

    add: アニメーションを追加

--- src/app/gallery-view.tsx
+++ src/app/gallery-view.tsx
@@ -2,12 +2,16 @@
 import Image from "next/image";
 import { artData } from "./art-data";
 import "./style.css";
+import { motion } from "framer-motion";
 
 export const GalleryView = () => {
   return (
-    <main className="bg-gray-900 flex flex-col items-center justify-center ">
-      <h1
-        className="text-white text-4xl mb-6 pt-50 pb-50 font-bold "
+    <main className="bg-gray-900 flex flex-col items-center justify-center">
+      <motion.h1
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="text-white text-4xl mb-6 pt-50 pb-50 font-bold"
         style={{
           fontFamily: "DM Serif Text",
           fontWeight: 400,
@@ -15,17 +19,25 @@ export const GalleryView = () => {
         }}
       >
         Gallery
-      </h1>
-      <div className="grid grid-cols-1 gap-4  ">
+      </motion.h1>
+
+      <div className="grid grid-cols-1 gap-4">
         {artData.map((src) => (
-          <Image
-            width={300}
-            height={300}
+          <motion.div
             key={src}
-            src={`/art/${src}`}
-            alt=""
-            className="hover:scale-105 transition-transform"
-          />
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.8 }}
+          >
+            <Image
+              width={300}
+              height={300}
+              src={`/art/${src}`}
+              alt=""
+              className="hover:scale-105 transition-transform"
+            />
+          </motion.div>
         ))}
       </div>
     </main>
